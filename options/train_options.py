141d2edf884379c778f1853b642fb33e1ea80d3a
yash12khandelwal <yash12khandelwal@gmail.com>
2021-03-13 10:53:58 +0000

    replaced too many args with a config

--- options/train_options.py
+++ options/train_options.py
@@ -3,6 +3,7 @@ import os
 import os.path as osp
 import datetime
 
+from config.cfg_parser import cfg_parser
 
 class TrainOptions():
     """ Training options for cmdline
@@ -38,58 +39,72 @@ class TrainOptions():
         """
 
         parser = argparse.ArgumentParser( description="training script for InterIIT Trafic Sign Recognition" )
-        parser.add_argument("--model", type=str, default='micronet', help="available options : dks/micronet")
-        parser.add_argument("--device", type=str, default='cpu', help="which GPU to use")
+        parser.add_argument(
+            "-v",
+            "--version",
+            type=str,
+            default="default.json",
+            help="name of the config file to use.",
+        )
+        parser.add_argument(
+            "-w", "--wandb", action="store_true", help="Log to wandb or not"
+        )
 
-        parser.add_argument("--snapshot-dir", type=str, default='checkpoints/logs',
-                            help="Where to save snapshots of the model.")
-        parser.add_argument("--data-dir", type=str, default='dataset/GTSRB',
-                            help="Path to the directory containing the source dataset.")
+        cfg = cfg_parser(join("config", args.version))
+        cfg.wandb = args.wandb
+        cfg.version = args.version
+        # parser.add_argument("--model", type=str, default='micronet', help="available options : dks/micronet")
+        # parser.add_argument("--device", type=str, default='cpu', help="which GPU to use")
 
-        parser.add_argument("--size", nargs='+', type=int,
-                            default=[32, 32], help="input image size.")
+        # parser.add_argument("--snapshot-dir", type=str, default='checkpoints/logs',
+        #                     help="Where to save snapshots of the model.")
+        # parser.add_argument("--data-dir", type=str, default='dataset/GTSRB',
+        #                     help="Path to the directory containing the source dataset.")
 
-        parser.add_argument("--batch-size", type=int,
-                            default=1, help="input batch size.")
-        parser.add_argument("--epochs", type=int, default=100,
-                            help="Number of training steps.")
-        parser.add_argument("--epochs-stop", type=int, default=100,
-                            help="Number of training steps for early stopping.")
-        parser.add_argument("--num-workers", type=int,
-                            default=4, help="number of threads.")
-        parser.add_argument("--learning-rate", type=float,
-                            default=2.5e-4, help="initial learning rate.")
-        parser.add_argument("--momentum", type=float, default=0.9,
-                            help="Momentum component of the optimiser.")
-        parser.add_argument("--weight-decay", type=float, default=0.0005,
-                            help="Regularisation parameter for L2-loss.")
-        parser.add_argument("--power", type=float, default=0.9,
-                            help="Decay parameter to compute the learning rate (only for deeplab).")
+        # parser.add_argument("--size", nargs='+', type=int,
+        #                     default=[32, 32], help="input image size.")
 
-        parser.add_argument("--num-classes", type=int, default=43,
-                            help="Number of classes for GTSRB.")
-        parser.add_argument("--init-weights", type=str,
-                            default=None, help="initial model.")
-        parser.add_argument("--restore-from", type=str, default=None,
-                            help="Where restore model parameters from.")
+        # parser.add_argument("--batch-size", type=int,
+        #                     default=1, help="input batch size.")
+        # parser.add_argument("--epochs", type=int, default=100,
+        #                     help="Number of training steps.")
+        # parser.add_argument("--epochs-stop", type=int, default=100,
+        #                     help="Number of training steps for early stopping.")
+        # parser.add_argument("--num-workers", type=int,
+        #                     default=4, help="number of threads.")
+        # parser.add_argument("--learning-rate", type=float,
+        #                     default=2.5e-4, help="initial learning rate.")
+        # parser.add_argument("--momentum", type=float, default=0.9,
+        #                     help="Momentum component of the optimiser.")
+        # parser.add_argument("--weight-decay", type=float, default=0.0005,
+        #                     help="Regularisation parameter for L2-loss.")
+        # parser.add_argument("--power", type=float, default=0.9,
+        #                     help="Decay parameter to compute the learning rate (only for deeplab).")
 
-        parser.add_argument("--save-pred-every", type=int, default=5, help="Save summaries and checkpoint every often.")
-        parser.add_argument("--print-freq", type=int, default=5, help="print loss and time fequency.")
+        # parser.add_argument("--num-classes", type=int, default=43,
+        #                     help="Number of classes for GTSRB.")
+        # parser.add_argument("--init-weights", type=str,
+        #                     default=None, help="initial model.")
+        # parser.add_argument("--restore-from", type=str, default=None,
+        #                     help="Where restore model parameters from.")
 
-        parser.add_argument("--wandb-api-key", type=str,
-                            default=None, help="Wandb API Key")
-        parser.add_argument("--wandb_id", type=str, default=None,
-                            help="Wandb run resume id (valid only if restore-from != None")
-        parser.add_argument("--wandb-name", type=str,
-                            default='', help="Name of the wandb run")
+        # parser.add_argument("--save-pred-every", type=int, default=5, help="Save summaries and checkpoint every often.")
+        # parser.add_argument("--print-freq", type=int, default=5, help="print loss and time fequency.")
+
+        # parser.add_argument("--wandb-api-key", type=str,
+        #                     default=None, help="Wandb API Key")
+        # parser.add_argument("--wandb_id", type=str, default=None,
+        #                     help="Wandb run resume id (valid only if restore-from != None")
+        # parser.add_argument("--wandb-name", type=str,
+        #                     default='', help="Name of the wandb run")
         
-        parser.add_argument("--class-weights",type=str,default=None,help="Class weights (numpy file)")
+        # parser.add_argument("--class-weights",type=str,default=None,help="Class weights (numpy file)")
 
-        parser.add_argument('--lr-decay-step', type=int, default=5, help="Step size for Learning rate decay")
+        # parser.add_argument('--lr-decay-step', type=int, default=5, help="Step size for Learning rate decay")
 
-        parser.add_argument("-s", "--seed", default=42, help="Random seed")
+        # parser.add_argument("-s", "--seed", default=42, help="Random seed")
 
-        return parser.parse_args()
+        return cfg
 
     def print_options(self, args):
         """ Function that prints and saves the output
