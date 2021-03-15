ccd37d876c424da4c72c26cb647e0d35142257bf
Siddhant Agarwal <agarwalsiddhant10@gmail.com>
2021-03-15 06:11:41 +0000

    Code cleaning

--- xai.py
+++ xai.py
@@ -20,39 +20,42 @@ import csv
 from PIL import Image 
 import matplotlib as mpl
 
-def rise_eval(model, device, dataloader, num_classes):
-        explainer = RISEBatch(model, (48, 48), gpu_batch=4)
-        # explainer.load_masks('masks.npy')
+def rise(model, dataloader, num_classes, image_size, save_path, device=torch.device("cuda:0")):
+        """
+        A function to create the saliency maps for all the images in the dataloader
+
+        Args:
+        model: The pretrained model (make sure to have it in eval mode)
+        dataloader: the dataloader whos images are to be used.
+        num_classes: number of output classes
+        save_path: the path to the directory where the images will be saved.
+        device: the device where you want to compute the maps.
+        """
+
+        #Create the explainer
+        explainer = RISEBatch(model, image_size, gpu_batch=20)
+
+        #Create the masks once, and save them. For later use, simply load the masks.
         explainer.generate_masks(N = 4000, s= 8, p1 = 0.1, savepath = 'masks.npy')
+        # explainer.load_masks('masks.npy')
 
-        klen = 11
-        ksig = 5
-        kern = gkern(klen, ksig)
-
-        blur = lambda x: torch.nn.functional.conv2d(x, kern, padding=klen//2)
-
-        insertion = CausalMetric(model, 'ins', 48*8, substrate_fn=blur, n_classes=num_classes, device=device)
-        deletion = CausalMetric(model, 'del', 48*8, substrate_fn=torch.zeros_like, n_classes=num_classes, device=device)
-
+        #Iterate over the dataloader
         for i, data in tqdm(enumerate(dataloader, 0), desc="Images" ):
+                #Currently stopping after 5 images
                 if i > 5:
                         break
+
+                #Extract the image and class from the data element
                 img = data[0]
-                print(img.shape)
                 img = img.float().to(device)
                 cl = data[1]
-                sal = explainer(img.to(device)).cpu().numpy()
-                print("Sal Shape : {}".format(sal.shape))
 
-                """
-                sal_batch = np.empty((1, sal.shape[2], sal.shape[3]))
-                sal_batch[0, :, :] = sal[:, cl, :, :]
-                scores2 = deletion.single_run(img.cpu(), sal_batch, verbose=0, save_to =None)
-                scores1 = insertion.single_run(img.cpu(), sal_batch, verbose=0, save_to = None)
-                """
+                #Generate the saliency map
+                sal = explainer(img.to(device)).cpu().numpy()
+
+                #Display the saliency map
 
                 img_cpu = img.clone().cpu().numpy()[0]
-                print(img_cpu.shape)
                 mean = [0.3337, 0.3064, 0.3171]
                 std =  [0.2672, 0.2564, 0.2629]
 
@@ -77,27 +80,9 @@ def rise_eval(model, device, dataloader, num_classes):
                 final_img = final_img.astype(np.uint8)
                 final_img = Image.fromarray(final_img)
 
-                final_img.save('/root/traffic_sign_interiit' + '/rise_' + str(i) + '.jpg')
-                # plt.imshow(img_cpu.astype(np.uint8))
-                # plt.imshow(sal_batch[0], alpha=0.5, cmap='jet')
-                # plt.axis('off')
-                # plt.savefig(args.image_path + '/rise_' + str(idx) + '.jpg')
-
-                # ins_auc = auc(scores1)
-                # del_auc = auc(scores2)
-                # ins_auc = 0
-                # del_auc = 0
-                # print("------------Summary (so far)----------------------------", flush=True)
+                final_img.save(save_path + '/rise_' + str(i) + '.jpg')
                 print("image: {}".format(i), flush = True)
-                # print("insertion score : ",ins_auc, flush = True)
-                # print("deletion score", del_auc, flush = True)
-                # print("--------------------------------------------------------", flush = True)
-        del explainer
-        explainer = None
-        # torch.cuda.empty_cache()
-        # return img_path, cl
-        # return img_path, ins_auc, del_auc, cl
-
+                
 opt = TrainOptions()
 args = opt.initialize()
 train_dataset = GTSRB(args, setname='train')
@@ -106,13 +91,4 @@ trainloader = get_loader(args, train_dataset)
 net, optimizer, schedular = model.CreateModel(args=args)
 net.load_state_dict(torch.load("/root/traffic_sign_interiit/opt_micronet_2021-3-11_11-30.pth"))
 net.eval()
-
-print("---")
-print(net)
-print(args)
-print("---")
-
-# x = torch.ones((4, 3, 48, 48))
-# x = x.float().cuda()
-# net(x)
-rise_eval(net, torch.device("cuda:0"), trainloader, 43)
+rise(net, trainloader, 43, (48, 48), "/root/traffic_sign_interiit/sample_outputs", torch.device("cuda:0"), )
